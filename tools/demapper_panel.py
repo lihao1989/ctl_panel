42d1bf41a84a40895eab79295c118efbb84e06eb
李皓 <345570600@qq.com>
2014-08-22 15:06:12 +0800

    修改界面帧号计算控件，增加刷新按钮，去掉不能用于tab切换的控件，修改调整绘图直接调用get_red函数

--- tools/demapper_panel.py
+++ tools/demapper_panel.py
@@ -24,7 +24,11 @@ sys.setdefaultencoding("utf-8")
 
 class MainFrame(wx.Frame):
     def __init__(self,parent,id):
-        wx.Frame.__init__(self, None, title=u"星座图绘制", size=(1000,700))
+        wx.Frame.__init__(self, None, title=u"星座图绘制")
+
+        self.panel = wx.Panel(self, -1, style = wx.RAISED_BORDER|wx.TAB_TRAVERSAL)
+        self.panel.SetBackgroundColour("white")
+
         self.Centre()
 
         self.SetBackgroundColour("white")
@@ -45,53 +49,59 @@ class MainFrame(wx.Frame):
         self.button1 = wx.Button(self, -1, "PCFI星座图")
         self.button1.SetBackgroundColour('black')
         self.button1.SetForegroundColour('white')
-        self.Bind(wx.EVT_BUTTON,self.draw_scatter,self.button1)
+        self.Bind(wx.EVT_BUTTON,self.draw_pcfich, self.button1)
 
         self.button2 = wx.Button(self, -1, "PDCCH星座图")
         self.button2.SetBackgroundColour('black')
         self.button2.SetForegroundColour('white')
-        self.Bind(wx.EVT_BUTTON,self.draw_scatter,self.button2)
+        self.Bind(wx.EVT_BUTTON, self.draw_pdcch, self.button2)
 
         self.button3 = wx.Button(self, -1, "PDSCH星座图")
         self.button3.SetBackgroundColour('black')
         self.button3.SetForegroundColour('white')
-        self.Bind(wx.EVT_BUTTON,self.draw_scatter,self.button3)
-
-        self.cb_grid = wx.CheckBox(self, -1, 
-            "显示网格",
-            style=wx.ALIGN_RIGHT)
-
-        #小区ID
-        id_cell = wx.StaticText(self, -1, u'小区ID:')
-        self.id_cell_t = wx.TextCtrl(self, -1, "0", style=wx.TE_READONLY)
-
-        #系统带宽
-        bandwidth = wx.StaticText(self, -1, u'系统带宽:')
-        self.bandwidth_t = wx.TextCtrl(self, -1, "0", style=wx.TE_READONLY)
+        self.Bind(wx.EVT_BUTTON,self.draw_pdsch, self.button3)
 
         #RNTI
-        RNTI_st = wx.StaticText(self, -1, u"RNTI:")
-        self.RNTI = wx.SpinCtrl(self, -1, "", (-1, -1))
+        RNTI_st = wx.StaticText(self.panel, -1, u"RNTI:")
+        self.RNTI = wx.SpinCtrl(self.panel, -1, "", (-1, -1))
         self.RNTI.SetRange(61,65523)
         self.RNTI.SetValue(100)
 
         #sfn
-        sfn_st = wx.StaticText(self, -1, u"子帧总数:")
-        self.sfn = wx.TextCtrl(self, -1, "", style=wx.TE_READONLY)
+        sfn_st = wx.StaticText(self.panel, -1, u"子帧总数:")
+        self.sfn = wx.TextCtrl(self.panel, -1, "", style=wx.TE_READONLY)
 
-        #sfn_scope
-        sfn_scope_st = wx.StaticText(self, -1, u"请输入子帧号的范围(上限：子帧总数):")
-        self.LOW = wx.TextCtrl(self, -1, "0",size=(80,30))
-        self.sfn_scope_zhi = wx.StaticText(self, -1, u"至")
-        self.HIGH = wx.TextCtrl(self, -1, "100")
+        #小区ID
+        id_cell = wx.StaticText(self.panel, -1, u'小区ID:')
+        self.id_cell_t = wx.TextCtrl(self.panel, -1, "0", style=wx.TE_READONLY)
+
+        #系统带宽
+        bandwidth = wx.StaticText(self.panel, -1, u'系统带宽:')
+        self.bandwidth_t = wx.TextCtrl(self.panel, -1, "0", style=wx.TE_READONLY)
+
+        #sfn_number
+        sfn_start_st = wx.StaticText(self.panel, -1, u"起始帧号:")
+        self.sfn_start = wx.TextCtrl(self.panel, -1, "0",size=(80,30))
+        sfn_total_st = wx.StaticText(self.panel, -1, u"子帧总数:")
+        self.sfn_total = wx.TextCtrl(self.panel, -1, "100")
+
+        #刷新处理按钮
+        self.refresh = wx.Button(self.panel, label="刷新")
+        self.Bind(wx.EVT_BUTTON,self.process,self.refresh)
+        self.refresh.SetBackgroundColour('black')
+        self.refresh.SetForegroundColour('white')
         
         #选择文件
-        self.button_file = wx.Button(self, label="选择")
+        self.button_file = wx.Button(self.panel, label="选择")
         self.Bind(wx.EVT_BUTTON,self.file_select,self.button_file)
+        self.button_file.SetBackgroundColour('black')
+        self.button_file.SetForegroundColour('white')
 
         #数据源
-        data_st = wx.StaticText(self, -1, u"数据源:")
-        self.data_t = wx.TextCtrl(self, -1, "", style=wx.TE_READONLY)
+        data_st = wx.StaticText(self.panel, -1, u"数据源:")
+        self.data_t = wx.TextCtrl(self.panel, -1, "", style=wx.TE_READONLY)
+
+        # self.cb_grid = wx.CheckBox(self.panel, -1, "显示网格", style=wx.ALIGN_RIGHT)
 
         ########开始布局############
         sizer1 = wx.BoxSizer(wx.HORIZONTAL)
@@ -118,26 +128,32 @@ class MainFrame(wx.Frame):
         sizer3.Add(data_st, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL)
         sizer3.Add(self.data_t, 0,wx.EXPAND)
         sizer3.Add(self.button_file, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL)
-        sizer3.Add(self.cb_grid, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL)
 
-        sizer4 = wx.FlexGridSizer(cols=4, hgap=5, vgap=5)
-        # sizer4.AddGrowableCol(1)
-        sizer4.Add(sfn_scope_st, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL)
-        sizer4.Add(self.LOW, 0,wx.EXPAND)
-        sizer4.Add(self.sfn_scope_zhi, 0,wx.ALIGN_CENTER_VERTICAL)
-        sizer4.Add(self.HIGH, 0,wx.EXPAND)
+        sizer4 = wx.FlexGridSizer(cols=7, hgap=5, vgap=5)
+        sizer4.AddGrowableCol(5)
+        sizer4.Add(sfn_start_st, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL)
+        sizer4.Add(self.sfn_start, 0,wx.EXPAND)
+        sizer4.Add((20,20))
+        sizer4.Add(sfn_total_st, 0,wx.ALIGN_CENTER_VERTICAL)
+        sizer4.Add(self.sfn_total, 0,wx.EXPAND)
+        sizer4.Add((20,20),wx.EXPAND)
+        sizer4.Add(self.refresh, 0,wx.ALIGN_RIGHT)
 
         sizer5 = wx.StaticBoxSizer(wx.StaticBox(self, wx.NewId(), u'星座图信息'), wx.VERTICAL)
         sizer5.Add(sizer2, 0, wx.EXPAND | wx.ALL, 10)
         sizer5.Add(sizer4, 0, wx.EXPAND | wx.ALL, 10)
         sizer5.Add(sizer3, 0, wx.EXPAND | wx.ALL, 10)
 
+        self.panel.SetSizer(sizer5)
+        sizer5.Fit(self)
+        sizer5.SetSizeHints(self)
+
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.sizer.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
         self.sizer.Add(wx.StaticLine(self), 0,wx.EXPAND|wx.TOP|wx.BOTTOM,5)
         self.sizer.Add(sizer1, 0, wx.EXPAND)
         self.sizer.Add(wx.StaticLine(self), 0,wx.EXPAND|wx.TOP|wx.BOTTOM,5)
-        self.sizer.Add(sizer5,0,wx.EXPAND)
+        self.sizer.Add(self.panel,0,wx.EXPAND)
         self.sizer.Add(self.toolbar, 0, wx.LEFT | wx.TOP | wx.GROW)
         self.SetSizer(self.sizer)
         self.Fit()
@@ -149,82 +165,68 @@ class MainFrame(wx.Frame):
                 "", wildcard, wx.OPEN)
         if file_path.ShowModal() == wx.ID_OK:
             self.data_t.SetValue(file_path.GetPath())
+            self.process(wx.EVT_BUTTON)
         file_path.Destroy()
-        self.process()
 
-    def process(self):
-        self.file = open(self.data_t.GetValue(),'rb')
-        self.file.seek(0,2)
-        end = self.file.tell()
+    def process(self,event):
+        self.fData = open(self.data_t.GetValue(),'rb')
+        self.fData.seek(0,2)
+        end = self.fData.tell()
         total_sfn = (end-8)/(8064)
         self.sfn.SetValue(str(total_sfn))
-        self.file.seek(0,0)
-        self.x = np.fromfile ( self.file , dtype = np.int32 , count = 2 )
-        self.id_cell_t.SetValue(str(self.x[0]))
-        self.bandwidth_t.SetValue(str(self.x[1]))
-        self.file.close()
+        self.fData.seek(0,0)
+        x = np.fromfile ( self.fData , dtype = np.int32 , count = 2 )
+        self.cell_id = x[0];
+        self.prb_count = x[1];
+        self.id_cell_t.SetValue(str(x[0]))
+        self.bandwidth_t.SetValue(str(x[1]))
 
-    def draw_scatter(self,event):
+        len_sf = self.prb_count*12*14;
+        sfn_start = int(self.sfn_start.GetValue())
+        N = int(self.sfn_total.GetValue())
+
+        self.dl_subframe_demapper = lte_sat.dl_subframe_demapper(int(self.RNTI.GetValue()))
+        self.dl_subframe_demapper.set_cell_id(int(self.cell_id))
+        self.dl_subframe_demapper.set_rb_count(int(self.prb_count))
+
+        """ Reconstruct the original complex array """
+        self.fData = open(self.data_t.GetValue(),'rb')
+        self.fData.seek(sfn_start*len_sf*2*4+8)
+        print self.fData.tell()
+        y = np.fromfile ( self.fData , dtype = np.complex64 , count = len_sf*N)
+
+        wfc = np.zeros ( [ N*len_sf ] , dtype = np.complex )
+        for i in range(len(y)):
+            wfc[i] = y[i]
+
+        self.dl_subframe_demapper.demux(wfc, (sfn_start%10))
+        self.fData.close();
+
+    def draw_pcfich(self, event):
+        self.draw_scatter(0)
+
+    def draw_pdcch(self, event):
+        self.draw_scatter(1)
+
+    def draw_pdsch(self, event):
+        self.draw_scatter(2)
+
+    def draw_scatter(self, drawtype):
         self.figure.clear() 
         self.axes = self.figure.add_subplot(111)
         self.axes.clear()
-        self.axes.grid(self.cb_grid.IsChecked())
-        try:
-            self.file = open(self.data_t.GetValue(),'rb')
-        except:
-            self.file_select(event)
-            self.file = open(self.data_t.GetValue(),'rb')
-        self.file.seek(8,1)
-        sfn_low = int(self.LOW.GetValue())
-        sfn_high = int(self.HIGH.GetValue())
-        N = sfn_high - sfn_low +1
-        len_sf = 1008
 
-        self.file.seek(sfn_low*len_sf*2*4,1)
-        print self.file.tell()
-        y = np.fromfile ( self.file , dtype = np.float32 , count = len_sf*2*N)
-        self.file.close()
+        pcfich_data = self.dl_subframe_demapper.get_red(drawtype)
 
-        real_list = []
-        imag_list = []
-        for i in range(len_sf*2*N):
-            if i%2==0:
-                real_list.append(y[i])
-            else:
-                imag_list.append(y[i])
+        pcfich_real = []
+        pcfich_imag = []
+        for i in range(len(pcfich_data)):
+            pcfich_real.append(pcfich_data[i].real)
+            pcfich_imag.append(pcfich_data[i].imag)
 
-        self.dl_subframe_demapper = lte_sat.dl_subframe_demapper(int(self.RNTI.GetValue()))
-        self.dl_subframe_demapper.set_cell_id(int(self.x[0]))
-        self.dl_subframe_demapper.set_rb_count(int(self.x[1]))
-        
-        real_total_list = zip(*[iter(real_list)]*len_sf)
-        imag_total_list = zip(*[iter(imag_list)]*len_sf)
-        # real_list.reshape(N,len_sf)
-        # imag_list.reshape(N,len_sf)
-        pcfich_total_real = []
-        pcfich_total_imag = []
-        for i in range(N):
-            """ Reconstruct the original complex array """
-            wfc = np.zeros ( [ len_sf ] , dtype = np.complex )
-            wfc.real = real_total_list[i]
-            wfc.imag = imag_total_list[i]
-            self.dl_subframe_demapper.demux(wfc, (i+sfn_low)%10)
-            # self.dl_subframe_demapper.demux(wfc, 0)
-            pcfich_data = self.dl_subframe_demapper.get_red(0)
-
-            pcfich_real = []
-            pcfich_imag = []
-            for i in range(len(pcfich_data)):
-                pcfich_real.append(pcfich_data[i].real)
-                pcfich_imag.append(pcfich_data[i].imag)
-
-                pcfich_total_real = pcfich_total_real + pcfich_real
-                pcfich_total_imag = pcfich_total_imag + pcfich_imag
-
-        self.axes.scatter( pcfich_total_real, pcfich_total_imag, facecolor='blue' )
+        self.axes.scatter( pcfich_real, pcfich_imag, facecolor='blue' )
         self.canvas.draw()
-
-    
+        
     def OnCloseWindow(self, event):
         self.Destroy()
 
